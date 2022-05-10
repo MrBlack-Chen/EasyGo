67309c765bce79335fe92862555c44df80ec1b7a
MrBlack-Chen <3072368402@qq.com>
2022-05-10 13:27:39 +0800

    Beta
    EasyGo1.0

---
+++ EasyGo2.0/resource.h
@@ -0,0 +1,75 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 EasyGo2.0.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_EASYGO20_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EASYGO20                    107
+#define IDI_SMALL                       108
+#define IDC_EASYGO20                    109
+#define IDR_MAINFRAME                   128
+#define IDR_WAVE1                       132
+#define IDR_WAVE2                       133
+#define IDR_WAVE3                       134
+#define IDR_WAVE4                       135
+#define IDR_WAVE5                       136
+#define IDR_WAVE6                       137
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_START                        32773
+#define ID_RANK                         32774
+#define ID_RANKING                      32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_ISCORING                     32779
+#define ID_EXIST                        32780
+#define ID_GRADE                        32781
+#define ID_32782                        32782
+#define ID_PAUSE                        32783
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_PRESERVE                     32786
+#define ID_32787                        32787
+#define ID_32788                        32788
+#define ID_DIFFICULTY                   32789
+#define ID_32790                        32790
+#define ID_32791                        32791
+#define ID_32792                        32792
+#define ID_32793                        32793
+#define ID_EASY                         32794
+#define ID_MIDDLE                       32795
+#define ID_HARD                         32796
+#define ID_SELF                         32797
+#define ID_                             32798
+#define ID_32799                        32799
+#define ID_DIFF                         32800
+#define ID_Menu                         32801
+#define ID_32802                        32802
+#define ID_HELP                         32803
+#define IDM_MESSAGE                     32804
+#define ID_32805                        32805
+#define ID_32806                        32806
+#define ID_32807                        32807
+#define ID_32808                        32808
+#define ID_music1                       32809
+#define ID_BGM1                         32810
+#define ID_BGM2                         32811
+#define ID_BGM3                         32812
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32813
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
